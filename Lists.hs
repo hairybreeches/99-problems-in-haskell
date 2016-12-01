691c78324137fc8df4a40ef3bae80ea5dff41a7b
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2016-12-01 16:08:04 +0000

    problem 6 (finding palindromes) and 7 (flattening a list)

--- Lists.hs
+++ Lists.hs
@@ -18,3 +18,12 @@ myLength (h:t) = 1 + myLength t
 myReverse :: [a] -> [a]
 myReverse [] = []
 myReverse (h:t) = myReverse t ++ [h]
+
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome x = x == (reverse x)
+
+data NestedList a = Elem a | List [NestedList a]
+
+flatten :: NestedList a -> [a]
+flatten (List xs) = concatMap flatten xs
+flatten (Elem x) = [x]
